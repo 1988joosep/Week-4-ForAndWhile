09b519304b6283062fd43f5c841ef7d5f231a333
Joosep Angast <joosep.angastrlive.com>
2024-09-25 16:15:34 +0300

    For and While

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Random rnd = new Random();
+
+int MyRandomnum;
+
+int randomSum = 0;
+
+
+for (int i = 1; i < 5;  i++)
+{
+   MyRandomnum = rnd.Next(0, 50);// 0 - 10
+    randomSum = randomSum + MyRandomnum;
+    Console.WriteLine($"My random number is: {MyRandomnum}");
+}
+Console.WriteLine($"Random sum total: {randomSum}");
---
+++ Week4Loops/Program.cs
@@ -0,0 +1,15 @@
+﻿//For - Loop
+//Dry - principle - Do not repeat yourself
+
+int i = 0;
+
+for (int i = 0; i < 5; i++) // i++ - i + 1
+{
+    Console.WriteLine("We don't need no education");
+}
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day!");
+//Console.WriteLine("We don't need no education");
+//Console.WriteLine("We don't need no education");
+//Console.WriteLine("We don't need no education");
+//Console.WriteLine("We don't need no education");
